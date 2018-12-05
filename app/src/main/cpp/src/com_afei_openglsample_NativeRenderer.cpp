02e929253fc932de071eca7bd644253bcdd7743d
zhangpengfei <zhangpengfei@sensetime.com>
2018-12-05 09:31:05 +0800

    add some comment

--- app/src/main/cpp/src/com_afei_openglsample_NativeRenderer.cpp
+++ app/src/main/cpp/src/com_afei_openglsample_NativeRenderer.cpp
@@ -18,9 +18,10 @@ JNIEXPORT void JNICALL Java_com_afei_openglsample_NativeRenderer_glInit
     if (g_program == GL_NONE) {
         LOGE("gl init failed!");
     }
+    // vPosition 是在 'vertex.vsh' 文件中定义的
     GLint g_position_handle =glGetAttribLocation(g_program, "vPosition");
     LOGD("g_position_handle: %d", g_position_handle);
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // RGBA (range: 0.0 ~ 1.0)
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // 背景颜色设置为黑色 RGBA (range: 0.0 ~ 1.0)
 }
 
 JNIEXPORT void JNICALL Java_com_afei_openglsample_NativeRenderer_glResize
@@ -30,11 +31,12 @@ JNIEXPORT void JNICALL Java_com_afei_openglsample_NativeRenderer_glResize
 
 JNIEXPORT void JNICALL Java_com_afei_openglsample_NativeRenderer_glDraw
         (JNIEnv *env, jobject instance) {
-    GLint vertexCount = 3; // 3个顶点
+    GLint vertexCount = 3;
+    // OpenGL的世界坐标系是 [-1, -1, 1, 1]
     GLfloat vertices[] = {
-            0.0f, 0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f
+            0.0f, 0.5f, 0.0f, // 第一个点（x, y, z）
+            -0.5f, -0.5f, 0.0f, // 第二个点（x, y, z）
+            0.5f, -0.5f, 0.0f // 第三个点（x, y, z）
     };
     glClear(GL_COLOR_BUFFER_BIT); // clear color buffer
     // 1. 选择使用的程序
@@ -42,6 +44,7 @@ JNIEXPORT void JNICALL Java_com_afei_openglsample_NativeRenderer_glDraw
     // 2. 加载顶点数据
     glVertexAttribPointer(g_position_handle, vertexCount, GL_FLOAT, GL_FALSE, 3 * 4, vertices);
     glEnableVertexAttribArray(g_position_handle);
+    // 3. 绘制
     glDrawArrays(GL_TRIANGLES, 0, vertexCount);
 }
 
